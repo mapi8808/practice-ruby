c9a7f88b5c40cdf17cc609196992dd4296757cea
mapi8808 <mapi.008.k@gmail.com>
2020-12-17 07:31:38 +0000

    [add]

--- type.rb
+++ type.rb
@@ -2,8 +2,6 @@ puts "Samの年齢は" + 27.to_s + "です"
 
 puts 100 + "200".to_i
 
-puts "WEBCAMPでプログラミング学習".include?("WEBCAMP")
-
 names = ["Git", "HTML", "CSS"]
 puts names[0]
 
@@ -16,3 +14,6 @@ puts "#{name}さんの体重は#{weight}です"
 
 tall = {:太郎=>185, :二郎=>170, :花子=>150}
 puts tall[:太郎]
+
+subjects = ["国語", "数学", "理科", "社会"]
+puts subjects [2]
